f77307ff0dd0f92d0b6a617638fc7d4010ea73b8
Guimaraes Neto <guimaraesneto@gmail.com>
2025-02-02 11:09:19 -0300

    Implementando servico de mensageria

--- src/main/java/br/com/ambevtech/ordermanager/external/kafka/OrderKafkaConsumer.java
+++ src/main/java/br/com/ambevtech/ordermanager/external/kafka/OrderKafkaConsumer.java
@@ -7,12 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class OrderKafkaConsumer {
+
     private final OrderService orderService;
+
     @KafkaListener(topics = "orders-topic", groupId = "order-group")
     public void processOrder(OrderRequestDTO order) {
         log.info("Pedido recebido via Kafka: {}", order);
